04b7998922e8782601d416f1f64bc07ab70c6b9c
毛亮 <wb-ml423659@alibaba-inc.com>
2019-06-17 11:32:58 +0800

    工厂模式

---
+++ src/main/java/com/louie/study/dp/factorymethod/example1/Client.java
@@ -0,0 +1,8 @@
+package com.louie.study.dp.factorymethod.example1;
+
+public class Client {
+	public static void main(String[] args) {
+		ExportOperate operate = new ExportOperate();
+		operate.export(2, "测试数据");
+	}
+}
