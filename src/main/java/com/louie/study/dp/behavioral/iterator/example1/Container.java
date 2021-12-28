30a3018071def177f1263d5d12b7c63724bba555
Louie <695871997@qq.com>
2021-12-28 19:22:49 +0800

    feat: 迭代器模式

---
+++ src/main/java/com/louie/study/dp/behavioral/iterator/example1/Container.java
@@ -0,0 +1,5 @@
+package com.louie.study.dp.behavioral.iterator.example1;
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ src/main/java/com/louie/study/dp/behavioral/iterator/example1/Iterator.java
@@ -0,0 +1,6 @@
+package com.louie.study.dp.behavioral.iterator.example1;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
