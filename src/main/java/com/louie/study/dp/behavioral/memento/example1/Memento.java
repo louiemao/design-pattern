383060645a89f7de4212cc50fa855e6434ec66aa
Louie <695871997@qq.com>
2021-12-28 19:44:11 +0800

    feat: 备忘录模式

---
+++ src/main/java/com/louie/study/dp/behavioral/memento/example1/Memento.java
@@ -0,0 +1,13 @@
+package com.louie.study.dp.behavioral.memento.example1;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+}
